9ba3ecb3b0dc0cfc5597e845b5368ac7d64d42c3
Bogdan Zavadovschi <bogdanzavadovschi17@gmail.com>
2022-05-28 16:14:23 +0300

    Arhitecture done

---
+++ src/main/java/com/bnz/core/JavaCompilerApplication.java
@@ -0,0 +1,13 @@
+package com.bnz.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCompilerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCompilerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bnz/core/JavaCompilerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnz.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCompilerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
